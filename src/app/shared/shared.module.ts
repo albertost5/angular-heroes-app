5c772bbf1b5690e10c75ebe559259f33d8ea7d6c
albertost5 <sancheztorresalberto@gmail.com>
2024-02-09 16:50:01 +0100

    Add folder structure: auth, heroes, shared and material

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+import {NotFoundComponent} from './pages/not-found/not-found.component';
+
+
+@NgModule({
+  declarations: [
+    NotFoundComponent
+  ],
+  imports: []
+})
+export class SharedModule {
+}
