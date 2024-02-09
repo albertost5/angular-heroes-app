1e766ee426ec499aa5301a16ae86e97d3058a87c
albertost5 <sancheztorresalberto@gmail.com>
2024-02-09 17:16:17 +0100

    Add routes for heroes and auth modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {NotFoundComponent} from './shared/pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule)
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent
+  },
+  {
+    path: '',
+    redirectTo: 'heroes',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
