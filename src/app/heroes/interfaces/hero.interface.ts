2b9b995a7ae4cc2b0168f9a88044adff107ad75a
albertost5 <sancheztorresalberto@gmail.com>
2024-02-23 17:36:56 +0100

    Add structure for new-hero page

--- src/app/heroes/interfaces/hero.interface.ts
+++ src/app/heroes/interfaces/hero.interface.ts
@@ -2,7 +2,7 @@ export interface Hero {
   id: string;
   superhero: string;
   publisher: Publisher;
-  alter_ego: string;
+  alter_ego:  string;
   first_appearance: string;
   characters: string;
   alt_img: string;
--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -6,5 +6,14 @@ import {Component} from '@angular/core';
   styles: []
 })
 export class NewPageComponent {
-
+  public publishers = [
+    {
+      id: 'DC Comics',
+      description: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      description: 'Marvel - Comics'
+    }
+  ];
 }
